6b287f3d9d564952149dbe7ca439d42e295ec1af
Cassius Cassity <levisept26@gmail.com>
2023-07-17 08:17:57 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { styled } from '@mui/system';
+import { Container, Typography, Button } from '@mui/material';
+
+const RootContainer = styled(Container)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  minHeight: '100vh',
+  padding: theme.spacing(4),
+}));
+
+const TitleTypography = styled(Typography)(({ theme }) => ({
+  marginBottom: theme.spacing(2),
+}));
+
+const ButtonStyled = styled(Button)(({ theme }) => ({
+  marginTop: theme.spacing(2),
+}));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RootContainer>
+      <TitleTypography variant="h3">
+        Welcome to My Trivia Game!
+      </TitleTypography>
+      <Typography variant="body1">
+        Get ready to test your knowledge with some trivia questions.
+      </Typography>
+      <ButtonStyled variant="contained" color="primary">
+        Start Game
+      </ButtonStyled>
+    </RootContainer>
   );
 }
 
